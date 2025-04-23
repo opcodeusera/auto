7fa63aa862b5a2076ce40e073f12b7c16468b63f
opcodeusera <149359617+opcodeusera@users.noreply.github.com>
2025-04-23 17:44:22 +0530

    Update bms.meta.js

--- bms.meta.js
+++ bms.meta.js
@@ -3,7 +3,7 @@
 // @namespace   test Scripts
 // @match       *://.*show.com/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      
 // @downloadURL https://github.com/opcodeusera/auto/blob/main/bms.js
 // @updateURL   https://github.com/opcodeusera/auto/blob/main/bms.meta.js
