4154facff1a211261e97275d4cd7bcb3c2ed607d
opcodeusera <149359617+opcodeusera@users.noreply.github.com>
2025-04-23 17:42:39 +0530

    Create bms.meta.js

---
+++ bms.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        test
+// @namespace   test Scripts
+// @match       *://.*show.com/*
+// @grant       none
+// @version     1.0
+// @author      
+// @downloadURL https://github.com/opcodeusera/auto/blob/main/bms.js
+// @updateURL   https://github.com/opcodeusera/auto/blob/main/bms.meta.js
+// @description 21/4/2025, 7:17:43 pm
+// ==/UserScript==
